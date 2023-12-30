bd27b316ad2876785c1a1fc397071f7a9a81c2db
FlytoTheSpace <flytothespace.64@gmail.com>
2023-12-30 15:45:58 +0530

    basics

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+print("Hello World")
+a = "hi!"
+
+#Comments
+
+print(15//3)
+
+"""
+InputStr = input("Calculate >")
+OutputStr = eval(InputStr);
+print(OutputStr)
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,7 @@
+const Squares = []
+
+for (let i = 0; i <10; i++) {
+    let a = i
+    console.log(`${a}^2 = ${a**2}`)
+    Squares.push(`${a}^2 = ${a**2}`)
+}
